b2b3477e1f6c0147987fc4126015f5f54ef076bc
HFS <HFS@HFS.example>
2022-11-11 11:59:40 +0100

    Fix gun

---
+++ Assets/Scripts/GoodEnemyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodEnemyController : MonoBehaviour, IShootable
+{
+    [SerializeField]
+    private GameObject BadVersion;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GetShot()
+    {
+        Instantiate(BadVersion, gameObject.transform.position, gameObject.transform.rotation);
+        Destroy(gameObject);
+    }
+}
